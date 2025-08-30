f51942df36d8f74f7dce5979026523a3e9d17a9d
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-30 12:22:43 +0600

    dom added to the login page

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// login button functionality 
+document.getElementById("login-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const mobileNumber = 12345678910;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        // alert("Login Successful");
+
+        // navigate to another page 
+        window.location.href = "./home.html"
+    }
+    else{
+        alert("Login Failed");
+    }
+
+
+})
